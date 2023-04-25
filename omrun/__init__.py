77ccbf95c3a4cee2f56bef3c376bb5b37b439e8c
ijknabla <ijknabla@gmail.com>
2023-04-25 18:42:06 +0900

    Implement BuiltModel

--- omrun/__init__.py
+++ omrun/__init__.py
@@ -1,14 +1,18 @@
+import re
 import sys
 from asyncio import Task, get_running_loop
-from collections.abc import Callable, Coroutine
+from collections.abc import Callable, Coroutine, Iterator
+from dataclasses import dataclass
 from functools import wraps
 from getpass import getuser
 from pathlib import Path
 from tempfile import gettempdir
-from typing import Any, ParamSpec, TypeVar, cast
+from typing import TYPE_CHECKING, Any, ParamSpec, TypeVar, cast
 
 from PySide6.QtCore import Slot
 
+if TYPE_CHECKING:
+    from typing_extensions import Self
 _P = ParamSpec("_P")
 _T = TypeVar("_T")
 
@@ -41,3 +45,37 @@ def get_omedit_work_directory() -> Path:
         return Path.home() / "AppData" / "Local" / "Temp" / "OpenModelica" / "OMEdit"
     else:
         return Path(gettempdir()) / f"OpenModelica_{getuser()}" / "OMEdit"
+
+
+@dataclass
+class BuiltModel:
+    executable: Path
+    init_xml: Path
+    info_json: Path
+
+    @property
+    def directory(self) -> Path:
+        return self.executable.parent
+
+    @classmethod
+    def from_directory(cls, directory: Path) -> Iterator["Self"]:
+        init_xml_stems = set(
+            re.sub(r"_init\.xml$", "", path.name) for path in directory.glob("*_init.xml")
+        )
+        info_json_stems = set(
+            re.sub(r"_info\.json$", "", path.name) for path in directory.glob("*_info.json")
+        )
+        stems = init_xml_stems & info_json_stems
+
+        for stem in stems:
+            for name in f"{stem}.exe", stem:
+                executable = directory / name
+                if executable.exists():
+                    break
+            else:
+                continue
+
+            init_xml = directory / f"{stem}_init.xml"
+            info_json = directory / f"{stem}_info.json"
+
+            yield cls(executable=executable, init_xml=init_xml, info_json=info_json)
