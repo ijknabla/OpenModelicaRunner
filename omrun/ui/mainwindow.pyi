53ac913802410395ea002b14124e3eb2bd28a39e
ijknabla <ijknabla@gmail.com>
2023-04-25 18:20:22 +0900

    Update type-hint

--- omrun/ui/mainwindow.pyi
+++ omrun/ui/mainwindow.pyi
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QGridLayout, QMainWindow, QWidget
+from PySide6.QtWidgets import QGridLayout, QMainWindow, QTreeWidget, QWidget
 
 class Ui_MainWindow(object):
     centralLayout: QGridLayout
+    modelTree: QTreeWidget
     def setupUi(self, MainWindow: QMainWindow) -> None: ...
