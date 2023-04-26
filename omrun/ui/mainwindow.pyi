bfbd9b171329c7e0ff558e038b075399524ede96
ijknabla <ijknabla@gmail.com>
2023-04-26 15:12:47 +0900

    Remove duplicate definition

--- omrun/ui/mainwindow.pyi
+++ omrun/ui/mainwindow.pyi
@@ -1,6 +1,5 @@
 from PySide6.QtWidgets import QGridLayout, QMainWindow, QWidget
 
 class Ui_MainWindow(object):
-    centralWidget: QWidget
     centralLayout: QGridLayout
     def setupUi(self, MainWindow: QMainWindow) -> None: ...
