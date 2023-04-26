ad4aafd1cfd3c6f628181a1ddb7f7d1a95f59b33
ijknabla <ijknabla@gmail.com>
2023-04-26 13:54:58 +0900

    Implement AsyncSlot

--- omrun/__init__.py
+++ omrun/__init__.py
@@ -1,4 +1,33 @@
-from typing import ParamSpec, TypeVar
+import sys
+from asyncio import Task, get_running_loop
+from collections.abc import Callable, Coroutine
+from functools import wraps
+from typing import Any, ParamSpec, TypeVar, cast
+
+from PySide6.QtCore import Slot
 
 _P = ParamSpec("_P")
 _T = TypeVar("_T")
+
+
+def AsyncSlot(
+    *args0: Any, **kwargs0: Any
+) -> Callable[[Callable[_P, Coroutine[Any, Any, _T]]], Callable[_P, None]]:
+    def decorator(f: Callable[_P, Coroutine[Any, Any, _T]]) -> Callable[_P, None]:
+        @Slot(*args0, **kwargs0)
+        @wraps(f)
+        def wrapper(*args1: _P.args, **kwargs1: _P.kwargs) -> None:
+            loop = get_running_loop()
+            task = loop.create_task(f(*args1, **kwargs1))
+            task.add_done_callback(_done_callback)
+
+        return cast(Callable[_P, None], wrapper)
+
+    return decorator
+
+
+def _done_callback(task: Task[_T]) -> None:
+    try:
+        task.result()
+    except Exception:
+        sys.excepthook(*sys.exc_info())
