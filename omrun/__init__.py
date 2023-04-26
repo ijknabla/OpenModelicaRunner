f1c0d37a096aa368decaf8421c8796c5e3010de3
ijknabla <ijknabla@gmail.com>
2023-04-26 13:34:46 +0900

    Deprecate unasync

--- omrun/__init__.py
+++ omrun/__init__.py
@@ -1,15 +1,4 @@
-from asyncio import run
-from collections.abc import Callable, Coroutine
-from functools import wraps
-from typing import Any, ParamSpec, TypeVar
+from typing import ParamSpec, TypeVar
 
 _P = ParamSpec("_P")
 _T = TypeVar("_T")
-
-
-def unasync(f: Callable[_P, Coroutine[Any, Any, _T]]) -> Callable[_P, _T]:
-    @wraps(f)
-    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _T:
-        return run(f(*args, **kwargs))
-
-    return wrapper
