ed1b8dc7dfedd503c9ecdfbb27e5ad9b96cd34f6
ijknabla <ijknabla@gmail.com>
2023-04-27 16:49:34 +0900

    Add SetupDialog

---
+++ omrun/ui/setupdialog.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'setupdialog.ui'
+##
+## Created by: Qt User Interface Compiler version 6.5.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QSizePolicy, QWidget)
+
+class Ui_SetupDialog(object):
+    def setupUi(self, SetupDialog):
+        if not SetupDialog.objectName():
+            SetupDialog.setObjectName(u"SetupDialog")
+        SetupDialog.resize(400, 300)
+
+        self.retranslateUi(SetupDialog)
+
+        QMetaObject.connectSlotsByName(SetupDialog)
+    # setupUi
+
+    def retranslateUi(self, SetupDialog):
+        SetupDialog.setWindowTitle(QCoreApplication.translate("SetupDialog", u"SetupDialog", None))
+    # retranslateUi
+
