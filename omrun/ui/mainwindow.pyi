5a4b2cca6adb95c33ffdf30aadf59cccf49a7d5c
ijknabla <ijknabla@gmail.com>
2023-04-27 11:43:52 +0900

    Add modelBrowserAction

--- omrun/ui/mainwindow.pyi
+++ omrun/ui/mainwindow.pyi
@@ -1,6 +1,8 @@
+from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QGridLayout, QListWidget, QMainWindow, QTreeWidget
 
 class Ui_MainWindow(object):
+    modelBroswerAction: QAction
     centralLayout: QGridLayout
     modelTree: QTreeWidget
     processWidget: QListWidget
