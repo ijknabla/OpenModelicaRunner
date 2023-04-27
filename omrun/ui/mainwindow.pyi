82b34ba4352813987b2f7634e43ab0c544e0d45f
ijknabla <ijknabla@gmail.com>
2023-04-27 12:48:05 +0900

    Put type-hint to modelBrowser

--- omrun/ui/mainwindow.pyi
+++ omrun/ui/mainwindow.pyi
@@ -1,9 +1,12 @@
 from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QGridLayout, QListWidget, QMainWindow, QTreeWidget
 
+from ..widget.modelbrowser import ModelBrowser
+
 class Ui_MainWindow(object):
     modelBroswerAction: QAction
     centralLayout: QGridLayout
     modelTree: QTreeWidget
     processWidget: QListWidget
+    modelBroswer: ModelBrowser
     def setupUi(self, MainWindow: QMainWindow) -> None: ...
