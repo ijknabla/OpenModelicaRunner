0146d9ed4a641f457608ddb9343515439c5ce240
ijknabla <ijknabla@gmail.com>
2023-04-27 13:22:42 +0900

    Add type-hint for widgets

--- omrun/ui/modelbrowser.pyi
+++ omrun/ui/modelbrowser.pyi
@@ -1,4 +1,7 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QLabel, QPushButton, QWidget
 
 class Ui_ModelBrowser(object):
+    workDirectoryLabel: QLabel
+    reloadPushButton: QPushButton
+    openPushButton: QPushButton
     def setupUi(self, ModelBrowser: QWidget) -> None: ...
