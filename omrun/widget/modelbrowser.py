7b79ad02ddc8398add4ab51a4470d1393151a8d7
ijknabla <ijknabla@gmail.com>
2023-04-27 12:37:09 +0900

    Call setupUi

--- omrun/widget/modelbrowser.py
+++ omrun/widget/modelbrowser.py
@@ -1,5 +1,14 @@
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QWidget
 
+from ..ui.modelbrowser import Ui_ModelBrowser
 
-class ModelBrowser(QWidget):
-    ...
+
+class ModelBrowser(Ui_ModelBrowser, QWidget):
+    def __init__(
+        self,
+        parent: QWidget | None = None,
+        f: Qt.WindowType = Qt.WindowType(0),
+    ) -> None:
+        super().__init__(parent=parent, f=f)
+        self.setupUi(self)
