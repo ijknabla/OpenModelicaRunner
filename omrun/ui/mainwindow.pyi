16bca03b2ec8be0e44ca5f5d4494ac9354165c9a
ijknabla <ijknabla@gmail.com>
2023-04-27 10:15:07 +0900

    Put type-hint for processWidget

--- omrun/ui/mainwindow.pyi
+++ omrun/ui/mainwindow.pyi
@@ -1,6 +1,7 @@
-from PySide6.QtWidgets import QGridLayout, QMainWindow, QTreeWidget, QWidget
+from PySide6.QtWidgets import QGridLayout, QListWidget, QMainWindow, QTreeWidget
 
 class Ui_MainWindow(object):
     centralLayout: QGridLayout
     modelTree: QTreeWidget
+    processWidget: QListWidget
     def setupUi(self, MainWindow: QMainWindow) -> None: ...
