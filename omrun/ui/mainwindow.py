b15773fe508d7a91c2ba6e9ed31c694dc45955a8
ijknabla <ijknabla@gmail.com>
2023-04-27 11:37:16 +0900

    Add View, Model Browser menu

--- omrun/ui/mainwindow.py
+++ omrun/ui/mainwindow.py
@@ -11,19 +11,24 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
 from PySide6.QtWidgets import (QApplication, QGridLayout, QHeaderView, QListWidget,
-    QListWidgetItem, QMainWindow, QMenuBar, QSizePolicy,
-    QStatusBar, QTreeWidget, QTreeWidgetItem, QWidget)
+    QListWidgetItem, QMainWindow, QMenu, QMenuBar,
+    QSizePolicy, QStatusBar, QTreeWidget, QTreeWidgetItem,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(800, 600)
+        self.action_Model_Broswer = QAction(MainWindow)
+        self.action_Model_Broswer.setObjectName(u"action_Model_Broswer")
+        self.action_Model_Broswer.setCheckable(True)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.centralLayout = QGridLayout(self.centralwidget)
@@ -42,10 +47,15 @@ class Ui_MainWindow(object):
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 800, 19))
+        self.menu_View = QMenu(self.menubar)
+        self.menu_View.setObjectName(u"menu_View")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menu_View.menuAction())
+        self.menu_View.addAction(self.action_Model_Broswer)
 
         self.retranslateUi(MainWindow)
 
@@ -54,8 +64,10 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.action_Model_Broswer.setText(QCoreApplication.translate("MainWindow", u"&Model Broswer", None))
         ___qtreewidgetitem = self.modelTree.headerItem()
         ___qtreewidgetitem.setText(1, QCoreApplication.translate("MainWindow", u"\u00a0", None));
         ___qtreewidgetitem.setText(0, QCoreApplication.translate("MainWindow", u"model", None));
+        self.menu_View.setTitle(QCoreApplication.translate("MainWindow", u"&View", None))
     # retranslateUi
 
