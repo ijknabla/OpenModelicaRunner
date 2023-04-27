7bc78db162513005af7be45ade695d7bf32de39c
ijknabla <ijknabla@gmail.com>
2023-04-27 13:12:16 +0900

    Add button

--- omrun/ui/modelbrowser.py
+++ omrun/ui/modelbrowser.py
@@ -15,20 +15,41 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QHeaderView, QSizePolicy,
-    QTreeWidget, QTreeWidgetItem, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QHeaderView, QLabel,
+    QPushButton, QSizePolicy, QTreeWidget, QTreeWidgetItem,
+    QVBoxLayout, QWidget)
 
 class Ui_ModelBrowser(object):
     def setupUi(self, ModelBrowser):
         if not ModelBrowser.objectName():
             ModelBrowser.setObjectName(u"ModelBrowser")
         ModelBrowser.resize(400, 300)
-        self.gridLayout = QGridLayout(ModelBrowser)
-        self.gridLayout.setObjectName(u"gridLayout")
+        self.verticalLayout = QVBoxLayout(ModelBrowser)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.workDirectoryLabel = QLabel(ModelBrowser)
+        self.workDirectoryLabel.setObjectName(u"workDirectoryLabel")
+
+        self.horizontalLayout.addWidget(self.workDirectoryLabel)
+
+        self.reloadPushButton = QPushButton(ModelBrowser)
+        self.reloadPushButton.setObjectName(u"reloadPushButton")
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.reloadPushButton.sizePolicy().hasHeightForWidth())
+        self.reloadPushButton.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout.addWidget(self.reloadPushButton)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
         self.treeWidget = QTreeWidget(ModelBrowser)
         self.treeWidget.setObjectName(u"treeWidget")
 
-        self.gridLayout.addWidget(self.treeWidget, 0, 0, 1, 1)
+        self.verticalLayout.addWidget(self.treeWidget)
 
 
         self.retranslateUi(ModelBrowser)
@@ -38,6 +59,8 @@ class Ui_ModelBrowser(object):
 
     def retranslateUi(self, ModelBrowser):
         ModelBrowser.setWindowTitle(QCoreApplication.translate("ModelBrowser", u"ModelBrowser", None))
+        self.workDirectoryLabel.setText(QCoreApplication.translate("ModelBrowser", u"/path/to/OMEdit", None))
+        self.reloadPushButton.setText(QCoreApplication.translate("ModelBrowser", u"reload", None))
         ___qtreewidgetitem = self.treeWidget.headerItem()
         ___qtreewidgetitem.setText(1, QCoreApplication.translate("ModelBrowser", u"\u00a0", None));
         ___qtreewidgetitem.setText(0, QCoreApplication.translate("ModelBrowser", u"model", None));
