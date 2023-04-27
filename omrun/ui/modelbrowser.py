dfa05dead44dce6f7ed8ada37286ae6e843b021d
ijknabla <ijknabla@gmail.com>
2023-04-27 12:32:37 +0900

    Add modelbrowser.ui

---
+++ omrun/ui/modelbrowser.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'modelbrowser.ui'
+##
+## Created by: Qt User Interface Compiler version 6.5.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
+
+class Ui_ModelBrowser(object):
+    def setupUi(self, ModelBrowser):
+        if not ModelBrowser.objectName():
+            ModelBrowser.setObjectName(u"ModelBrowser")
+        ModelBrowser.resize(400, 300)
+
+        self.retranslateUi(ModelBrowser)
+
+        QMetaObject.connectSlotsByName(ModelBrowser)
+    # setupUi
+
+    def retranslateUi(self, ModelBrowser):
+        ModelBrowser.setWindowTitle(QCoreApplication.translate("ModelBrowser", u"ModelBrowser", None))
+    # retranslateUi
+
